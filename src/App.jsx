16fbdc512132cc0bdf54cda6987d660d95e5b055
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-11 08:24:28 +0200

    "NavBar" Class Component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Hello from :</h1>
+                <p>"NavBar" Class Component</p>
+            </React.Fragment>
+        )
+    }
+    /* 
+    <div>
+        <h1>Hello from :</h1>
+        <p>"NavBar" Class Component</p>
+    </div> 
+    */
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
+// import { NavBar } from "./App";
+import NavBar from "./App";
 
-var element = <button class="btn btn-danger">Hello React World !</button>;
+// var element = <button class="btn btn-danger">Hello React World !</button>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(element);
+// root.render(element);
+root.render(<NavBar />);
 
-console.log(element);
+// console.log(element);
