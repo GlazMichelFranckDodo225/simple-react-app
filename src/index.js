879bed1066cdab9e35114e964a00c9658539604f
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-15 11:35:37 +0200

    Nested Components

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 // import { NavBar } from "./App";
-import NavBar from "./App";
 import './index.css';
+import App from "./App";
 
 // var element = <button class="btn btn-danger">Hello React World !</button>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(element);
-root.render(<NavBar />);
+root.render(<App />);
 
 // console.log(element);
