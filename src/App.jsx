7fd0d7de94b39d2764e9822fb7774599d8d0e7d0
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-15 11:23:11 +0200

    Fix Error - DOM property class

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,30 @@ export default class NavBar extends Component {
     render() {
         return (
             <React.Fragment>
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">My App</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="/#">My App</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Home</a>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/#">Home</a>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </nav>
                 {/* <h1>Hello from :</h1>
-                <p className="hightlight">"NavBar" Class Component</p> */}
+                <p classNameName="hightlight">"NavBar" className Component</p> */}
             </React.Fragment>
         )
     }
     /* 
     <div>
         <h1>Hello from :</h1>
-        <p>"NavBar" Class Component</p>
+        <p>"NavBar" className Component</p>
     </div> 
     */
 }
