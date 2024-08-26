feccfe9d1b477065fb01b8c068959bf206be1006
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-26 06:33:11 +0200

    Handle Events

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import MainContent from './components/MainContent'
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default class App extends Component {
   render() {
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 
 export default class MainContent extends Component {
   // state = {appTitle: "Customers"};
-  state = {appTitle: "Employees"};
+  // state = {appTitle: "Employees"};
+  state = {
+    pageTitle: "Customers",
+    customersCount: 5
+  };
 
   render() {
     return (
       <div>
         {/* <h4>Customers</h4> */}
-        <h4 className='border-bottom m-1 p-1'>{this.state.appTitle}</h4>
+        <h4 className='border-bottom m-1 p-1'>{this.state.pageTitle}</h4>
+        <p>{this.state.customersCount}</p>
+        <button className='btn btn-info' onClick={this.onRefreshClicked}>Refresh</button>
       </div>
     )
+  }
+
+  onRefreshClicked() {
+    console.log("Refresh Button Clicked !");
   }
 }
