4012eefee3bd65e50356c33ba3b3222b3c30b046
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-26 06:14:50 +0200

    Render Expressions

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 
 export default class MainContent extends Component {
+  // state = {appTitle: "Customers"};
+  state = {appTitle: "Employees"};
+
   render() {
     return (
-      <div>MainContent</div>
+      <div>
+        {/* <h4>Customers</h4> */}
+        <h4 className='border-bottom m-1 p-1'>{this.state.appTitle}</h4>
+      </div>
     )
   }
 }
