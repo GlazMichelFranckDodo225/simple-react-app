069239f279e19a036b166b68fedbd200efd24059
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-26 06:53:28 +0200

    Update Class Component State

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import MainContent from './components/MainContent'
-import 'bootstrap/dist/css/bootstrap.css';
 
 export default class App extends Component {
   render() {
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -12,14 +12,17 @@ export default class MainContent extends Component {
     return (
       <div>
         {/* <h4>Customers</h4> */}
-        <h4 className='border-bottom m-1 p-1'>{this.state.pageTitle}</h4>
+        <h4>{this.state.pageTitle}</h4>
         <p>{this.state.customersCount}</p>
-        <button className='btn btn-info' onClick={this.onRefreshClicked}>Refresh</button>
+        <button onClick={this.onRefreshButtonClicked}>Refresh</button>
       </div>
     )
   }
 
-  onRefreshClicked() {
-    console.log("Refresh Button Clicked !");
+  onRefreshButtonClicked = () => {
+    // console.log("Refresh Button Clicked !");
+    this.setState({
+      customersCount: 7
+    });
   }
 }
